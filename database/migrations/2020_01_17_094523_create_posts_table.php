b2d4b960638543d983e0ef7c7237fcba76ae3b8b
Николай <talam0nal@gmail.com>
2020-01-17 13:18:44 +0300

    [миграция] Таблица для постов

---
+++ database/migrations/2020_01_17_094523_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->longText('title');
+            $table->string('subtitle')->nullable();
+            $table->string('preview')->nullable();
+            $table->longText('text')->nullable();
+            $table->integer('category')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
