cb2ac56ab11ad923abf0a0d1d48e9fcba4913e1c
Николай <talam0nal@gmail.com>
2020-01-20 20:13:38 +0300

    Ресурсный роут для категорий

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('posts', 'PostController');
+	Route::resource('categories', 'CategoryController');
 	Route::post('upload-editor', 'PostController@uploadEditor')->name('image.editor');
 });
