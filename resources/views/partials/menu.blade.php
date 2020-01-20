c7d238f428d5456871ec7f68c0b48b13ce138567
Николай <talam0nal@gmail.com>
2020-01-20 20:12:14 +0300

    Меню вынесено в отдельный файл

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,13 @@
+<aside class="col-sm-3">
+    <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action active">
+            Публикации
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            Комментарии
+        </a>
+        <a href="{{ route('categories.index') }}" class="list-group-item list-group-item-action">
+            Категории
+        </a>
+    </div>
+</aside>
