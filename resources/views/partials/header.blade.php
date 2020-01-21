0e9aa6ecc2fa339dc01c83753cd32e14b02db6e6
Николай <talam0nal@gmail.com>
2020-01-21 14:57:46 +0300

    Вынес хедер в отдельный файл

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<div class="container main-menu">
+    <div class="row">
+        <nav class="col-sm-12">
+            <a href="/" class="logo">Blog</a>
+            <a href="#">Категории</a>
+            <form class="form-inline search-input">
+                <input class="form-control mr-sm-2" type="search" placeholder="Поиск по сайту">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Найти</button>
+            </form>
+            @auth
+                <a href="{{ route('posts.create') }}" class="btn btn-success">Написать статью</a>
+                @else
+                <a href="{{ route('login') }}">Войти</a>
+            @endauth
+        </nav>
+    </div>
+</div>
--- resources/views/site/main.blade.php
+++ resources/views/site/main.blade.php
@@ -1,30 +1,14 @@
 @extends ('layouts.layout')
 
 @section ('content')
-	<div class="container main-menu">
-		<div class="row">
-			<nav class="col-sm-12">
-				<a href="/" class="logo">Blog</a>
-				<a href="#">Категории</a>
-				<form class="form-inline search-input">
-					<input class="form-control mr-sm-2" type="search" placeholder="Поиск по сайту">
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Найти</button>
-				</form>
-				@auth
-					<a href="{{ route('posts.create') }}" class="btn btn-success">Написать статью</a>
-					@else
-					<a href="{{ route('login') }}">Войти</a>
-				@endauth
-			</nav>
-		</div>
-	</div>
+    @include ('partials.header')
 
-	<div class="container content">
-		<div class="row">
-			<div class="col-12">
-				@foreach ($posts as $post)
-					<div class="post">
-						<a href="{{ route('posts.show', $post->id) }}" class="title">{{ $post->title }}</a>
+    <div class="container content">
+        <div class="row">
+            <div class="col-12">
+                @foreach ($posts as $post)
+                    <div class="post">
+                        <a href="{{ route('posts.show', $post->id) }}" class="title">{{ $post->title }}</a>
                         @if ($post->preview)
                             <div class="row image-gap">
                                 <div class="col-sm-6">
@@ -33,24 +17,24 @@
                             </div>
                         @endif
                         <div class="row">
-                        	<div class="col-12">
-                        		Мне нравится (0) Просмотров (0)
-                        		<br>
-                        		{{ $post->created_at }}
-                        		<br>
-                        		@if ($post->category)
-                        			{{ $post->category->title }}
-                        		@endif
-                        	</div>
+                            <div class="col-12">
+                                Мне нравится (0) Просмотров (0)
+                                <br>
+                                {{ $post->created_at }}
+                                <br>
+                                @if ($post->category)
+                                    {{ $post->category->title }}
+                                @endif
+                            </div>
                         </div>
-					</div>
-				@endforeach
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-12">
-				{{ $posts->links() }}
-			</div>
-		</div>
-	</div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                {{ $posts->links() }}
+            </div>
+        </div>
+    </div>
 @endsection
